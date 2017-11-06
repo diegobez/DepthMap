b8c1e1bca3f092b45022c7acda85b99417088bcb
Enrique Moltó <kike.molto@gmail.com>
2017-11-06 08:50:06 +0100

    Now compile okey and commented

--- DepthMap/DepthMap.cpp
+++ DepthMap/DepthMap.cpp
@@ -39,8 +39,13 @@ using namespace std;
 ///////////////////////////////////////////////////////////////////////////////////
 int main()
 {
-	IplImage* img1 = cvLoadImage("imgR.png");
-	IplImage* img2 = cvLoadImage("imgL.png");
+
+	//Loading stereo images
+
+	IplImage* img1 = cvLoadImage("resources/images/imgR.png");
+	IplImage* img2 = cvLoadImage("resources/images/imgL.png");
+
+	//Creating images with cv
 
 	IplImage *rimage = cvCreateImage(
 		cvSize(img1->width, img1->height), IPL_DEPTH_8U, 1);
@@ -50,22 +55,40 @@ int main()
 		cvSize(img2->width, img2->height), IPL_DEPTH_8U, 1);
 	cvCvtColor(img2, limage, CV_RGB2GRAY);
 
+	//Showing stereo images in different windows
+
 	cvNamedWindow("Right", CV_WINDOW_AUTOSIZE);
 	cvShowImage("Right", rimage);
 
 	cvNamedWindow("Left", CV_WINDOW_AUTOSIZE);
 	cvShowImage("Left", limage);
 
+
+	//Create image matrix (rows,cols,type_depth)
+	//It returns a matrix
+	//CV_8UC1 = 8bits unsigned 1 channel
+	//CV_16S = 	16bit signed
+
 	CvMat *matr = cvCreateMat(rimage->height, rimage->width, CV_8UC1);
 	CvMat *matl = cvCreateMat(limage->height, limage->width, CV_8UC1);
-	CvMat* disp = cvCreateMat(rimage->height, rimage->width, CV_16S);
+
+	//The output single - channel 16 - bit signed disparity map of the same size as input images
+	CvMat* disp = cvCreateMat(rimage->height, rimage->width, CV_16S);  
 	CvMat* vdisp = cvCreateMat(rimage->height, rimage->width, CV_16S);
+
+	//ansign rimage and limage to matr and mat1 (new matrix) wich are 8bits unsigned
 
 	cvConvert(rimage, matr);
 	cvConvert(limage, matl);
 
+	//CvStereoBMState is created (where is the method cvCreateStereoBMstate?)
+
 	CvStereoBMState *BMState = cvCreateStereoBMState();
+
 	assert(BMState != 0);
+
+	//Adding features to CvStereoBMState
+
 	BMState->preFilterSize = 21;
 	BMState->preFilterCap = 31;
 	BMState->SADWindowSize = 21;
@@ -74,11 +97,19 @@ int main()
 	BMState->textureThreshold = 10;
 	BMState->uniquenessRatio = 15;
 
-
+	//find stereo correspondence (leftImage,rightImage, ounput 17 bit signed disparity map, stereo correspondence)
+	//mat and mat1 must be 8bits unsigned and disp must be 16bit signed
+	
 	cvFindStereoCorrespondenceBM(matr, matl, disp, BMState);
-	cvNormalize(disp, vdisp, 0, 255, CV_MINMAX);
 
-	cvShowImage("depthmap", vdisp);
+	//Normalize image and save it in vdisp
+	cvNormalize(disp, vdisp, 0, 255, CV_MINMAX, CV_8U);
+
+	//Showing depthmap image
+	
+	cvShowImage("Dethmap", vdisp);
 	cvWaitKey(0);
+
+
 	return 0;
 }
