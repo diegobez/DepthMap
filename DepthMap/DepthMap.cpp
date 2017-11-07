cdd9a6cc8913720412d2b06d1874c561eb180f36
Enrique Moltó <kike.molto@gmail.com>
2017-11-07 08:56:57 +0100

    example with also sgbm algorithm

--- DepthMap/DepthMap.cpp
+++ DepthMap/DepthMap.cpp
@@ -43,15 +43,18 @@ int main()
 
 
 	//-- 1. Read the images
-	Mat imgLeft = imread("resources/images/myLimage.png", IMREAD_GRAYSCALE);
-	Mat imgRight = imread("resources/images/myRimage.png", IMREAD_GRAYSCALE);
+	Mat imgLeft = imread("resources/images/imgL.png", IMREAD_GRAYSCALE);
+	Mat imgRight = imread("resources/images/imgR.png", IMREAD_GRAYSCALE);
 
 	imshow("LeftImage", imgLeft);
 	imshow("RightImage", imgRight);
 
-	//-- And create the image in which we will save our disparities
+	//-- And create the image in which we will save our disparitiess
 	Mat imgDisparity16S = Mat(imgLeft.rows, imgLeft.cols, CV_16S);
 	Mat imgDisparity8U = Mat(imgLeft.rows, imgLeft.cols, CV_8UC1);
+
+	Mat new_imgDisparity16S;
+	Mat new_imgDisparity8U = Mat(imgLeft.rows, imgLeft.cols, CV_8UC1);
 
 	if (imgLeft.empty() || imgRight.empty())
 	{
@@ -68,7 +71,7 @@ int main()
 	BMState->textureThreshold = 10;
 	BMState->uniquenessRatio = 15;*/
 
-	int ndisparities = 0;   /**< Range of disparity. Must be divisible by 16.  */
+	int ndisparities = 128;   /**< Range of disparity. Must be divisible by 16.  */
 							/*The disparity range depends on the distance between
 							the two cameras and the distance between the cameras and the object of interest. 
 							Increase the DisparityRange when the cameras are far apart or the objects 
@@ -82,13 +85,18 @@ int main()
 	sbm->setTextureThreshold(10);
 	sbm->setUniquenessRatio(15);
 
+	Ptr<StereoSGBM> ssgbm = StereoSGBM::create(0,ndisparities, SADWindowSize,0, 0,0,0,0,0,1,false);
+
 	//-- 3. Calculate the disparity image
 	sbm->compute(imgLeft, imgRight, imgDisparity16S);
+	ssgbm->compute(imgLeft, imgRight, new_imgDisparity16S);
 
 	//-- Check its extreme values
 	double minVal; double maxVal;
+	double new_minVal; double new_maxVal;
 
 	minMaxLoc(imgDisparity16S, &minVal, &maxVal);
+	minMaxLoc(new_imgDisparity16S, &new_minVal, &new_maxVal);
 
 	printf("Min disp: %f\n Max value: %f \n", minVal, maxVal);
 
@@ -97,9 +105,16 @@ int main()
 	namedWindow("Disparity", CV_WINDOW_AUTOSIZE);
 	imshow("Disparity", imgDisparity8U);
 
+	new_imgDisparity16S.convertTo(new_imgDisparity8U, CV_8UC1, 255 / (maxVal - new_minVal));
+	namedWindow("New_Disparity", CV_WINDOW_AUTOSIZE);
+	imshow("New_Disparity", new_imgDisparity8U);
+
+
 	//-- 5. Save the image
 	const std::string finalname ("NewImage");
 	imwrite("resources/images/"+finalname+".png", imgDisparity16S);
+
+
 
 	waitKey(0);
 
